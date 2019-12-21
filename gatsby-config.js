b12cb077c08dbb307519820b02ae3830148dae35
iamchanii <iamchanii.0310@gmail.com>
2019-12-21 17:48:25 +0900

    3. JSON 플러그인 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,14 @@ module.exports = {
         plugins: ['gatsby-tinacms-git'],
       },
     },
+    'gatsby-tinacms-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    'gatsby-transformer-json',
   ],
 };
