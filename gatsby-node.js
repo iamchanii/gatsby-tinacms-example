afe198df9482bed5c783682a8bb446e2b7182cc6
iamchanii <iamchanii.0310@gmail.com>
2019-12-21 20:05:14 +0900

    6. JSON 데이터 내의 마크다운 처리하기

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,33 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.onCreateNode = ({
+  node,
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode, createNodeField } = actions;
 
-// You can delete this file if you're not using it
+  if (node.internal.type === 'PagesJson') {
+    node.description = node.description || '';
+
+    const textNode = {
+      id: createNodeId(`${node.id} markdown field`),
+      children: [],
+      parent: node.id,
+      internal: {
+        content: node.description,
+        mediaType: `text/markdown`, // Important!
+        contentDigest: createContentDigest(node.description),
+        type: `${node.internal.type}Markdown`,
+      },
+    };
+
+    createNode(textNode);
+
+    // Add link to the new node
+    createNodeField({
+      node,
+      name: `markdownContent___NODE`, // Before the ___NODE: Name of the new fields
+      value: textNode.id, // Connects both nodes
+    });
+  }
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,34 @@ import SEO from '../components/SEO';
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query($slug: String) {
-      dataJson(path: { eq: $slug }) {
+      pagesJson(path: { eq: $slug }) {
         title
         fileRelativePath
         rawJson
+        fields {
+          markdownContent {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
       }
     }
   `);
 
-  const [formData] = useLocalJsonForm(data.dataJson, {
+  const [formData] = useLocalJsonForm(data.pagesJson, {
     fields: [
       {
         label: '타이틀',
         description: '기존 Hi People 위치에 표시됩니다.',
         name: 'rawJson.title',
         component: 'text',
+      },
+      {
+        label: '설명',
+        description: '본문에 표시됩니다.',
+        name: 'rawJson.description',
+        component: 'markdown',
       },
     ],
   });
@@ -30,6 +43,11 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" />
       <h1>{formData.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: formData.fields.markdownContent.childMarkdownRemark.html,
+        }}
+      />
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
     </Layout>
